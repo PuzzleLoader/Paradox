36e0e7c662849652b9447ad0e2014708e81507e8
repletsin5 <50048371+repletsin5@users.noreply.github.com>
2024-10-15 21:54:58 +0100

    Fix for pre6

--- src/main/java/com/github/puzzle/paradox/game/event/BlockEventEvents.java
+++ src/main/java/com/github/puzzle/paradox/game/event/BlockEventEvents.java
@@ -2,6 +2,7 @@ package com.github.puzzle.paradox.game.event;
 
 import com.github.puzzle.paradox.core.event.Event;
 import com.github.puzzle.paradox.core.event.EventFactory;
+import finalforeach.cosmicreach.blockevents.BlockEventArgs;
 import finalforeach.cosmicreach.blockevents.BlockEventTrigger;
 import finalforeach.cosmicreach.blocks.BlockState;
 import finalforeach.cosmicreach.world.Zone;
@@ -10,15 +11,15 @@ import java.util.Map;
 
 public class BlockEventEvents {
 
-    public static final Event<OnBlockEventAct> ON_BLOCK_EVENT_ACT = EventFactory.createArrayBacked(OnBlockEventAct.class, callbacks -> (trigger,srcBlockState, zone, args) -> {
+    public static final Event<OnBlockEventAct> ON_BLOCK_EVENT_ACT = EventFactory.createArrayBacked(OnBlockEventAct.class, callbacks -> (args) -> {
         for (OnBlockEventAct callback : callbacks) {
             if (callback != null)
-                callback.onBlockEvenAct(trigger, srcBlockState, zone, args);
+                callback.onBlockEvenAct(args);
         }
     });
 
     @FunctionalInterface
     public interface OnBlockEventAct {
-        void onBlockEvenAct(BlockEventTrigger trigger, BlockState srcBlockState, Zone zone, Map<String, Object> args);
+        void onBlockEvenAct(BlockEventArgs args);
     }
 }
