987958050255e07e3fb0e63b21c77246d61fa2cc
repletsin5 <50048371+repletsin5@users.noreply.github.com>
2024-10-26 21:27:31 +0100

    Forgotten file

---
+++ src/main/java/com/github/puzzle/paradox/game/server/packets/PuzzleGamePacket.java
@@ -0,0 +1,7 @@
+package com.github.puzzle.paradox.game.server.packets;
+
+public class PuzzleGamePacket {
+
+    public boolean shouldGameIgnore = false;
+    protected static final org.slf4j.Logger LOGGER =  org.slf4j.LoggerFactory.getLogger("Cosmic Reach | Game packet");
+}
